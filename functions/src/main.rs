b5fc7808a334896e61f6e1c3323ca59e40d54f20
Suryadev78 <pandeysuryadev00@gmail.com>
2024-10-18 23:32:12 +0530

    wrote add and subtract functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,8 +2,11 @@ fn main() {
     let a = 5;
     let b = 10;
     let result = add(a,b);
+    let subtraction = subtract(a,b);
     println!("Result: {}", result);
 }
 fn add(a:i32,b:i32)->i32{
     return a+b;
 }
+fn subtract(a:i32,b:i32)->i32{
+    return a-b; }
