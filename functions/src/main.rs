d42bea0a77077c66a1be1ce5a9b8ad272c4c68ed
Suryadev78 <pandeysuryadev00@gmail.com>
2024-10-20 23:56:50 +0530

    added a new function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,24 +1,24 @@
 fn main() {
     let a = 23;
     let b = 24;
-   let isEven = IsEven(a,b);
-   let square = square(a);
+    check_even_odd(a, b);
+    let square_a = square(a);
+    println!("Square of {} is {}", a, square_a);
 }
-fn IsEven(a:i32,b:i32){
-    if(a%2==0){
-        println!("{} is even",a);
-    }
-    else{
-        println!("{} is odd",a)
-    }
-    if (b%2==0){
-    println!("{} is even",b);
-   }
-   else {
-    println!("{} is odd",b);
 
-   }
+fn check_even_odd(a: i32, b: i32) {
+    if a % 2 == 0 {
+        println!("{} is even", a);
+    } else {
+        println!("{} is odd", a);
+    }
+    if b % 2 == 0 {
+        println!("{} is even", b);
+    } else {
+        println!("{} is odd", b);
+    }
 }
-fn square(a:i32){
-    return a*a;
+
+fn square(a: i32) -> i32 {
+    a * a
 }
