3f0b66fc9d1a16c35be7c46bad004e5323a309bd
Suryadev78 <pandeysuryadev00@gmail.com>
2024-10-22 21:14:24 +0530

    added new function

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     println!("Sum of squares of {} and {} is {}", a, b, sum);
     let average = average(a, b);
     println!("Average of {} and {} is {}", a, b, average);
+    let difference = difference(a, b);
+    println!("Difference of {} and {} is {}", a, b, difference);
 }
 
 fn check_even_odd(a: i32, b: i32) {
@@ -33,4 +35,8 @@ fn sum_of_squares(a: i32, b: i32) -> i32 {
 
 fn average(a: i32, b: i32) -> f32 {
     (a + b) as f32 / 2.0
+}
+
+fn difference(a: i32, b: i32) -> i32 {
+    a - b
 }
